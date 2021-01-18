506318f88cd43b994edf1da2bd1757372a7a37ac
unknown <coolgood88142@gmail.com>
2021-01-19 04:06:30 +0800

    :rocket: 重新部署heroku

--- app/Http/Controllers/Api/LineBotController.php
+++ app/Http/Controllers/Api/LineBotController.php
@@ -9,45 +9,54 @@ use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\SignatureValidator;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use GuzzleHttp\Client;
+use Carbon\Carbon;
+use Config;
 use Exception;
 class LineBotController extends Controller
 {
     public function getMessageWeather(Request $request){
-        $accessToken = 'b9z+6EeBQ6i4D9iHJKTgy4oMznvpwYJqllWaigLXXzhm7xx8/WYTsa9zLyyj5Rim2r2m+T/axKZaoRMVeaK+L7ME8RFRYr9xMRhXsh2H+nKazDoe0VP6TikA8B9SYXTRFbXuHAGbA/vIWA0+i43kdAdB04t89/1O/w1cDnyilFU=';
-        $channelSecret = 'b9d3f9583e6f74c1a9a276bb7bd17ae2';
+        $httpClient = new CurlHTTPClient(env('LINEBOT_TOKEN'));
+        $bot = new LINEBot($httpClient, ['channelSecret' => env('LINEBOT_SECRET')]);
+        $weatherUrl = 'https://opendata.cwb.gov.tw/api';
+        $token = 'CWB-96170F0C-F4B6-4626-B946-D6892DA6D584';
+        $client = new \GuzzleHttp\Client();
+        $cityData = Config::get('city');
+        $weathers = Config::get('weather');
 
-        // $signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
-        // if (!SignatureValidator::validateSignature($request->getContent(), $channelSecret, $signature)) {
-           
-        //     return;
-        // }
+        $startTime = 6;
+        $endTime = 18;
+        $today = Carbon::now()->timezone('Asia/Shanghai');
+        $hour = (int)$today->format('H');
+        $type = 0;
 
-        // $httpClient = new CurlHTTPClient ($accessToken);
-        // $lineBot = new LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+        if($hour > $endTime){
+            $type = 2;
+        }else if($hour <= $startTime){ 
+            $type = 1;
+        }
 
-        // try {
-          
-        //     $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+        $text = $request->events[0]['text'];
+        $pop = '目前無降雨機率資料';
+        foreach($cityData as $city){
+            foreach($city as $key => $value){
+                if($key == $text){
+                    $locationName = urlencode($key);
+                    $url = $weatherUrl  . $weathers[0] . '?Authorization=' . $token . '&locationName=' . $locationName;
 
-        //     foreach ($events as $event) {
-                
-        //         $replyToken = $event->getReplyToken();
-        //         $text = $event->getText();// 得到使用者輸入
-        //         $lineBot->replyText($replyToken, $text);// 回復使用者輸入
-        //         //$textMessage = new TextMessageBuilder("你好");
-        //       //  $lineBot->replyMessage($replyToken, $textMessage);
-        //     }
-        // } catch (Exception $e) {
-           
-        //     return;
-        // }
+                    $response = $client->get($url);
+                    $json = json_decode($response->getBody());
+                    $data = $json->records;
+                    $pop = $text . '降雨機率為：';
+                    $pop = $pop . $data->location[0]->weatherElement[1]->time[$type]->parameter->parameterName;
+                    $pop = $pop . '%';
+                }
+            }
+        }
 
-        // return;
 
-        $httpClient = new CurlHTTPClient($accessToken);
-        $bot = new LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-        $text = $request->events[0]['message']['text'];
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+        
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($pop);
 
         $response = $bot->replyMessage($request->events[0]['replyToken'], $textMessageBuilder);
         if ($response->isSucceeded()) {
