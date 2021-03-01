a26924809ceb845ae616dd27c46ac996b6df0a90
unknown <coolgood88142@gmail.com>
2021-03-01 12:07:14 +0800

    :rocket: 重新佈署 heroku

--- resources/js/components/vision/edit.vue
+++ resources/js/components/vision/edit.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="modal" id="exampleModal" tabindex="-1" role="dialog">
+    <div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
